864ad5730a9dc780ab8301e69c82297de61cc6a2
Farhan Bagwan <141229921+Farhan00987@users.noreply.github.com>
2026-02-16 12:51:25 +0530

    Var vs let vs const

---
+++ now.js
@@ -0,0 +1,24 @@
+// if (true){
+//     const HiddenSecret = "Shh!";
+// }
+// console.log(HiddenSecret)
+
+// let accountID = 1234567;
+// let email = "farhan@gmai.com";
+
+// if (true) {
+//     var city = "Pune"
+// }
+// accountID = 89564
+// email = "fahran@gmail.com"
+// console.table([city, accountID, email] )
+
+
+const accountID = 897456;
+let email = "farhan.bagban@gmail.com"
+if(true){
+    let city = "Pune"
+}
+accountID = 7845145;
+email = "farhan@gmai.com"
+console.log(city)
